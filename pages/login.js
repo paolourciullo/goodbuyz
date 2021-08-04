4a090fe214dda8135d88d44e992baddd1d1ab3d3
Paolo U <paolourciullo@gmail.com>
2021-08-04 14:31:43 -0400

    add login auth

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   List,
   Typography,
@@ -10,12 +10,24 @@ import {
 import Layout from '../components/Layout'
 import useStyles from '../utils/styles'
 import NextLink from 'next/link'
+import axios from 'axios'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const classes = useStyles()
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post('/api/users/login', { email, password })
+      alert('successful login')
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message)
+    }
+  }
   return (
     <Layout title="Login">
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
@@ -27,6 +39,7 @@ export default function Login() {
               id="email"
               label="Email"
               inputProps={{ type: 'email' }}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
 
@@ -37,6 +50,7 @@ export default function Login() {
               id="password"
               label="Password"
               inputProps={{ type: 'password' }}
+              onChange={(e) => setPassword(e.target.value)}
             ></TextField>
           </ListItem>
 
